8b1fc28434134d615a4cac71abd301bb85ef5abb
Rakibul md <rakib245875@gmail.com>
2022-04-18 04:52:37 +0600

    fixed reload to login issue

--- src/Pages/Login/RequireAuth/RequireAuth.js
+++ src/Pages/Login/RequireAuth/RequireAuth.js
@@ -2,10 +2,14 @@ import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Navigate, useLocation } from "react-router-dom";
 import auth from "../../../firebase.init";
+import Banner from "../../Home/Banner/Banner";
 
 const RequireAuth = ({ children }) => {
     const [user, loading, error] = useAuthState(auth);
     const location = useLocation();
+    if (loading) {
+        return <div>Loading</div>;
+    }
 
     if (!user) {
         return (
